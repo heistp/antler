5b7fe5c90050c81318dad5de4a56f18744ec62e2
Pete Heist <pete@heistp.net>
2022-11-07 21:17:17 -0500

    Add cwnd.

--- charts_tcpinfo.go
+++ charts_tcpinfo.go
@@ -88,16 +88,8 @@ func (n *ChartsTCPInfo) data(san []streamAnalysis) (data chartsData) {
 			l = ll
 		}
 		h.addColumn(l)
+		h.addColumn(l)
 	}
-	/*
-		for _, d := range san {
-			l := string(d.Client.Flow)
-			if ll, ok := n.FlowLabel[d.Client.Flow]; ok {
-				l = ll
-			}
-			h.addColumn(l)
-		}
-	*/
 	data.addRow(h)
 	for i, d := range san {
 		for _, n := range d.TCPInfo {
@@ -106,15 +98,12 @@ func (n *ChartsTCPInfo) data(san []streamAnalysis) (data chartsData) {
 			for j := 0; j < len(san); j++ {
 				if j != i {
 					r.addColumn(nil)
+					r.addColumn(nil)
 					continue
 				}
 				r.addColumn(n.TotalRetransmits)
+				r.addColumn(n.SendCwnd)
 			}
-			/*
-				for j := 0; j < len(san); j++ {
-					r.addColumn(nil)
-				}
-			*/
 			data.addRow(r)
 		}
 	}

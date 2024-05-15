125dcd07a4a9f35372a00479e4dd867f3e0bc3d2
Pete Heist <pete@heistp.net>
2024-05-15 15:52:58 +0200

    minor comment update for multiWriteCloser

--- io.go
+++ io.go
@@ -5,8 +5,9 @@ package antler
 
 import "io"
 
-// multiWriteCloser provides an analog to io.MultiWriter for WriteClosers,
-// returning a Writer that
+// multiWriteCloser returns an io.MultiWriter that uses the given WriteClosers
+// as the Writers, so that writes to the returned Writer are duplicated to each
+// of the given WriteClosers.
 func multiWriteCloser(wc ...io.WriteCloser) io.Writer {
 	var ww []io.Writer
 	for _, w := range wc {

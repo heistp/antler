a830c32b920d63dd4f37eed942d2550138c136c9
Pete Heist <pete@heistp.net>
2024-05-15 15:55:39 +0200

    wip: work on types and methods for executing Tests in Groups

--- group.go
+++ group.go
@@ -3,6 +3,8 @@
 
 package antler
 
+import "context"
+
 // Group is used to form a hierarchy of Tests. Each Group is a node in the
 // hierarchy containing a list of Tests with the same ID keys, and a list of
 // sub-Groups.
@@ -18,15 +20,52 @@ type Group struct {
 	// Group lists any sub-Groups of the Group.
 	Group []Group
 
-	// During is a pipeline of Reports run while the Test runs.
+	// During is a pipeline of Reports run while the Tests run.
 	During Report
 
-	// DuringDefault is the default pipeline of Reports run while the Test runs.
+	// DuringDefault is the default pipeline of Reports run while the Tests run.
 	DuringDefault Report
 
-	// After is a pipeline of Reports run after the Test completes.
+	// After is a pipeline of Reports run after the Tests complete.
 	After Report
 
-	// After is the default pipeline of Reports run after the Test completes.
+	// After is the default pipeline of Reports run after the Tests complete.
 	AfterDefault Report
+}
+
+// VisitTests calls the given visitor func for each Test in the Group
+// hierarchy. The visitor may return false to stop visiting, in which case
+// VisitTests will also return false.
+func (g *Group) VisitTests(visitor func(*Test) bool) bool {
+	for _, t := range g.Test {
+		if !visitor(&t) {
+			return false
+		}
+	}
+	for _, g := range g.Group {
+		if !g.VisitTests(visitor) {
+			return false
+		}
+	}
+	return true
+}
+
+// do runs a doer on the Tests, and recursively on the sub-Groups.
+func (g *Group) do(ctx context.Context, d doer2) (err error) {
+	for _, t := range g.Test {
+		if err = d.do(ctx, &t); err != nil {
+			return
+		}
+	}
+	for _, g := range g.Group {
+		if err = g.do(ctx, d); err != nil {
+			return
+		}
+	}
+	return
+}
+
+// A doer2 takes action on a Test, visited in a Group hierarchy.
+type doer2 interface {
+	do(context.Context, *Test) error
 }

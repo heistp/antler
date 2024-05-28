80410b06c601dbe513350f9765d5146019b8f8f6
Pete Heist <pete@heistp.net>
2024-05-28 15:58:48 +0200

    wip: start work on the run2 command for Groups

--- antler.go
+++ antler.go
@@ -27,6 +27,11 @@ func Run(ctx context.Context, cmd Command) error {
 	return cmd.run(ctx)
 }
 
+// Run2 runs an Antler Command.
+func Run2(ctx context.Context, cmd Command) error {
+	return cmd.run(ctx)
+}
+
 // A Command is an Antler command.
 type Command interface {
 	run(context.Context) error
@@ -262,6 +267,187 @@ func teeReport(ctx context.Context, src reporter, test *Test, rw rwer,
 			err = e
 		}
 	}
+	return
+}
+
+// Run2Command runs tests and reports.
+type Run2Command struct {
+	// Filter selects which Tests to run. If Filter is nil, Tests which were not
+	// run before or had errors are run.
+	Filter TestFilter
+
+	// Skipped is called when a Test was skipped because it wasn't accepted by
+	// the Filter.
+	Skipped func(*Test)
+
+	// ReRunning is called when a Test is being re-run because the prior result
+	// contains errors.
+	ReRunning func(*Test)
+
+	// Linked is called when Test data was linked from a prior run.
+	Linked func(*Test)
+
+	// Running is called when a Test starts running.
+	Running func(*Test)
+
+	// Done is called when the RunCommand is done.
+	Done func(RunInfo)
+}
+
+// run implements command
+func (r Run2Command) run(ctx context.Context) (err error) {
+	var c *Config
+	if c, err = LoadConfig(&load.Config{}); err != nil {
+		return
+	}
+	var rw resultRW
+	if rw, err = c.Results.open(); err != nil {
+		return
+	}
+	d := doRun2{r, rw, &RunInfo{}}
+	defer func() {
+		d.Info.Elapsed = time.Since(d.Info.Start)
+		if d.Info.Ran == 0 {
+			if e := rw.Abort(); e != nil && err == nil {
+				err = e
+			}
+		} else {
+			var e error
+			if d.Info.ResultDir, e = rw.Close(); e != nil && err == nil {
+				err = e
+			}
+		}
+		if r.Done != nil {
+			r.Done(*d.Info)
+		}
+	}()
+	d.Info.Start = time.Now()
+	err = c.Run.do(ctx, d, reportStack{})
+	return
+}
+
+// doRun2 is a doer that runs a Test and its reports.
+type doRun2 struct {
+	Run2Command
+	RW   resultRW
+	Info *RunInfo
+}
+
+// do implements doer
+func (u doRun2) do(ctx context.Context, test *Test, rst reportStack) (
+	err error) {
+	rw := test.RW(u.RW)
+	var s reporter
+	if u.Filter != nil {
+		if !u.Filter.Accept(test) {
+			if s, err = u.link(test); err != nil {
+				return
+			}
+			if s == nil {
+				if u.Skipped != nil {
+					u.Skipped(test)
+				}
+				return
+			} else {
+				if u.Linked != nil {
+					u.Linked(test)
+				}
+				u.Info.linked()
+			}
+		}
+	} else if test.DataFile != "" {
+		if s, err = u.link(test); err != nil {
+			return
+		}
+		if s != nil {
+			var e bool
+			if e, err = test.DataHasError(rw); err != nil {
+				return
+			}
+			if e {
+				if u.ReRunning != nil {
+					u.ReRunning(test)
+				}
+				s = nil
+			} else {
+				if u.Linked != nil {
+					u.Linked(test)
+				}
+				u.Info.linked()
+			}
+		}
+	}
+	if s == nil {
+		if u.Running != nil {
+			u.Running(test)
+		}
+		u.Info.ran()
+		if s, err = u.run(ctx, test); err != nil {
+			return
+		}
+	}
+	err = teeReport(ctx, s, test, rw, rst)
+	return
+}
+
+// run runs a Test.
+func (u doRun2) run(ctx context.Context, test *Test) (src reporter, err error) {
+	rw := test.RW(u.RW)
+	var w io.WriteCloser
+	if w, err = test.DataWriter(rw); err != nil {
+		if _, ok := err.(DataFileUnsetError); !ok {
+			return
+		}
+		err = nil
+	}
+	var a appendData
+	var p report = test.DuringDefault.report()
+	if w != nil {
+		p = append(p, writeData{w})
+	} else {
+		p = append(p, &a)
+	}
+	d := make(chan any, dataChanBufLen)
+	ctx, x := context.WithCancelCause(ctx)
+	defer x(nil)
+	go node.Do(ctx, &test.Run, &exeSource{}, d)
+	for e := range p.pipeline(ctx, d, nil, rw) {
+		x(e)
+		if err == nil {
+			err = e
+		}
+	}
+	if err != nil {
+		return
+	}
+	if w != nil {
+		var r io.ReadCloser
+		if r, err = test.DataReader(rw); err != nil {
+			return
+		}
+		src = readData{r}
+	} else {
+		src = rangeData(a)
+	}
+	return
+}
+
+// link hard links the DataFile and FileRefs from the prior Test run, and
+// returns a source reporter for the report pipeline. If there is no prior Test
+// run or DataFile, the returned src and err are both nil.
+func (u doRun2) link(test *Test) (src reporter, err error) {
+	rw := test.RW(u.RW)
+	if err = test.LinkPriorData(rw); err != nil {
+		if errors.Is(err, fs.ErrNotExist) {
+			err = nil
+		}
+		return
+	}
+	var r io.ReadCloser
+	if r, err = test.DataReader(rw); err != nil {
+		return
+	}
+	src = readData{r}
 	return
 }
 
